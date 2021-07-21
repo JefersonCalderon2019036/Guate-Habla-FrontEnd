69c8bb99a7b69e0c721ee6a89192d14e479346ff
Erick <edeleon-2019598@kinal.edu.gt>
2021-07-21 13:43:34 -0600

    register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './componetes/inicio/inicio.component';
 import { PrincipalComponent } from './componetes/principal/principal.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   {path: "inicio", component: InicioComponent},
   {path: "principal", component: PrincipalComponent},
   {path: "login", component: LoginComponent},
+  {path: "register", component: RegisterComponent},
   {path: "**", component: InicioComponent}
 ];
 
