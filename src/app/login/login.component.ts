26f7a1adb5d9ee374852c3b2120916b2f328d270
JefersonCalderon <jcalderon-2019036@kinal.edu.gt>
2021-07-22 16:05:35 -0600

    6 commit
    se realizo la conexión así la función iniciar sección, con la re dirección hacia la pagina principal

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { usuarios } from '../modelos/user.modelos';
+import { UsersServices } from '../servicios/user.services';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  public usuarioModel: usuarios;
+  public token: any;
+  public identidad: any;
+  public rol: any;
 
-  constructor() { }
+  constructor(
+    private _usuarioService: UsersServices,
+    private _router: Router
+  ) {
+    this.usuarioModel  = new usuarios("","","","","","","","",0,"","")
+  }
+
 
   ngOnInit(): void {
+  }
+
+  login() {
+    this._usuarioService.login(this.usuarioModel).subscribe(
+      (response) => {
+        this.token = response.token;
+        this.identidad = response.user;
+        localStorage.setItem('token', this.token);
+        localStorage.setItem('Rol', this.identidad.rol);
+        localStorage.setItem("nombre", this.identidad.name);
+        localStorage.setItem("imagendemiperfil", this.identidad.img);
+        localStorage.setItem("iddelusuario", this.identidad._id);
+        this._router.navigate(['/principal'])
+      },
+      (error) => {
+        console.log(<any>error);
+      }
+    );
   }
 
 }
