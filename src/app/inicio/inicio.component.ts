9c8a85f088d795d8c8969b0aa8db7f46acfecb13
JefersonCalderon <jcalderon-2019036@kinal.edu.gt>
2021-08-02 09:01:33 -0600

    solución de errores

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,18 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersServices } from '../servicios/user.services';
 import { ScriptsService } from "./../scripts.service"
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
-  styleUrls: ['./inicio.component.scss']
+  styleUrls: ['./inicio.component.scss'],
+  providers: [UsersServices]
 })
 export class InicioComponent implements OnInit {
+  public rol: any;
 
-  constructor( private _CargaScripts:ScriptsService)
+  constructor( public _usuarioService: UsersServices, private _CargaScripts:ScriptsService)
    {
      _CargaScripts.Carga(["inicio/inicio"]);
+     this.rol = this._usuarioService.getRol();
     }
 
   ngOnInit(): void {
+    if(this.rol = null){
+      localStorage.setItem('token', "");
+        localStorage.setItem('Rol', "");
+        localStorage.setItem("nombre", "");
+        localStorage.setItem("imagendemiperfil", "");
+        localStorage.setItem("iddelusuario", "");
+        localStorage.setItem("idchat", "")
+    }
   }
 
 }
